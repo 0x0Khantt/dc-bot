893cf03ea09ad63fb516b034b4d8426b71d5592c
Smurf Cat <khantkhantt404@gmail.com>
2025-08-16 00:32:03 +0630

    Initial commit for Railway

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
+// Create a new Discord client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// When bot is ready
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
+
+    // Set custom activity
     client.user.setActivity('Hacking Mainframe', { type: ActivityType.Watching });
 });
 
-client.login(process.env.BOT_TOKEN);
+// Login with the token from .env
+client.login(process.env.BOT_TOKEN)
+    .then(() => console.log('Bot logged in successfully!'))
+    .catch(err => console.error('Login failed:', err));
